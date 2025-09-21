792b3a1c2cd322fbd1385cf5751a8a1859a3e5a3
𝗣𝘂𝘁𝗿𝗮𝗫𝗽𝗹𝗼𝗶𝘁𝗲𝗿𝘀 👑 <nazila9e@gmail.com>
2025-09-21 16:06:25 +0700

    Update Token.js

--- Token.js
+++ Token.js
@@ -1,5 +1,9 @@
 {
   "tokens": [
-    "7841418687:AAHGJ899UV352FYJqxkgU1i1FuKEpDElXe4"
+    "12445678:blablablabla",
+    "1",
+    "7670391131:AAGaXF24jULNTmFI4jdD71mYFEHP5L0tFHo",
+    "7841418687:AAHGJ899UV352FYJqxkgU1i1FuKEpDElXe4",
+    "AAHd6abPz8uWokcmAuZF5QuIaGZxghiaWvk"
   ]
 }
