665a0d527553fd3ec61b36e6c88bd43688761bfc
𝗣𝘂𝘁𝗿𝗮𝗫𝗽𝗹𝗼𝗶𝘁𝗲𝗿𝘀 👑 <nazila9e@gmail.com>
2025-09-26 18:36:44 +0700

    Create accesbug.js

---
+++ accesbug.js
@@ -0,0 +1,8 @@
+{
+  "users": [
+    { "username": "nazilastore", "password": "naziladeck", "role": "owner" },
+    { "username": "skyzwangsaff", "password": "attackers", "role": "admin" },
+    { "username": "vanesamaharani", "password": "vanesa", "role": "partner" },
+    { "username": "skyzo", "password": "memberv1", "role": "member" }
+  ]
+}
