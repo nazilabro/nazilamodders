1dfcb79ad7314607c63b657866df60a5b83b2270
𝗣𝘂𝘁𝗿𝗮𝗫𝗽𝗹𝗼𝗶𝘁𝗲𝗿𝘀 👑 <nazila9e@gmail.com>
2025-09-26 22:36:10 +0700

    Add user anastayaherlia (owner) via Zetrox Panel

--- accesbug.js
+++ accesbug.js
@@ -1,8 +1,28 @@
 {
   "users": [
-    { "username": "nazilastore", "password": "naziladeck", "role": "owner" },
-    { "username": "skyzwangsaff", "password": "attackers", "role": "admin" },
-    { "username": "vanesamaharani", "password": "vanesa", "role": "partner" },
-    { "username": "skyzo", "password": "memberv1", "role": "member" }
+    {
+      "username": "nazilastore",
+      "password": "naziladeck",
+      "role": "owner"
+    },
+    {
+      "username": "skyzwangsaff",
+      "password": "attackers",
+      "role": "admin"
+    },
+    {
+      "username": "vanesamaharani",
+      "password": "vanesa",
+      "role": "partner"
+    },
+    {
+      "username": "skyzo",
+      "password": "memberv1",
+      "role": "member"
+    },
+    {
+      "username": "anastayaherlia",
+      "role": "owner"
+    }
   ]
 }
