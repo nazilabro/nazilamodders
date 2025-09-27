998b45cfcc1d561fe59643fb0991823cae590c50
𝗣𝘂𝘁𝗿𝗮𝗫𝗽𝗹𝗼𝗶𝘁𝗲𝗿𝘀 👑 <nazila9e@gmail.com>
2025-09-27 16:55:18 +0700

    Delete token.js

--- token.js
+++
@@ -1,9 +0,0 @@
-{
-  "tokens": [
-    "12445678:blablablabla",
-    "1",
-    "7670391131:AAGaXF24jULNTmFI4jdD71mYFEHP5L0tFHo",
-    "7841418687:AAHGJ899UV352FYJqxkgU1i1FuKEpDElXe4",
-    "AAHd6abPz8uWokcmAuZF5QuIaGZxghiaWvk"
-  ]
-}
